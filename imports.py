17aba812c3482106970d317db2ab02f03732c604
lawlikeed <lawlikeed@gmail.com>
2022-11-02 02:13:47 -0500

    cleaned up imports

---
+++ imports.py
@@ -0,0 +1,6 @@
+from ctypes import sizeof
+import itertools
+from math import floor
+import random
+import os
+from team import Team
--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,19 @@
-import itertools
-from math import floor
-import random
-from team import Team
+from imports import *
+
+def round_robin(team_list):
+    for i in range(1, num_of_days+1):  
+        prev = ('-1', '-1')  
+        print('-----Day',i,'line up-----')
+        for j in range(int(total_matches/num_of_days)):
+            # Choose a random tuple from list of matches
+            match = random.choice(team_list)
+            # Display first team in tuple
+            print(match)  
+            # Update list  
+            team_list.remove(match)      
+            prev = match
+
+
 
 num_of_teams = 4#int(input ('How many teams?: '))
 num_of_days = 2#int(input ('How many days?: '))
@@ -18,36 +30,28 @@ for i in range(num_of_teams):
 print('All Registered Teams')
 for team in list_of_teams: 
     print('*'+team.name)
-
+print('')
 
 # Build all teams opponents list and output list
 for team in list_of_teams:
     team.create_ops(list_of_teams)
     print(team.name+"'s","Oppenent List:", team.opponents)
-
+print('')
 
 # Create list of team numbers
 team_numbers_list = list(range(1, num_of_teams+1))
 
 # Get all combos of team match ups 
-test_list = list(itertools.combinations(team_numbers_list, 2))
-print(test_list)
+original_list = list(itertools.combinations(team_numbers_list, 2))
+print(original_list , '\n')
 
 # Display matche line ups
-for i in range(1, num_of_days+1):  
-    prev = '-1'  
-    print('-----Day',i,'line up-----')
-    for j in range(int(total_matches/num_of_days)):
-        match = random.choice(test_list)
-        print(match[0])
-        print(prev)
-        while match[0] == prev:
-            print('entering while loop')
-            match = random.choice(test_list)
-        test_list.remove(match)        
-        prev = match
-        print(match)
-        print(prev)
+while True:
+    round_robin(original_list.copy())
+    cont = input('Would you like to generate a new schedule? (Y/N): ')
+    if cont.upper() == 'N':
+        break
+    os.system('cls')
 
 
 
@@ -55,16 +59,3 @@ for i in range(1, num_of_days+1):
 
 
 
-
-
-
-# print('-----Day 1 line up-----')
-# for i in range(int(total_matches/num_of_days)):
-#     match = random.choice(test_list)
-#     test_list.remove(match)
-#     print(match)
-# print('-----Day 2 line up-----')
-# for i in range(int(total_matches/num_of_days)):
-#     match = random.choice(test_list)
-#     test_list.remove(match)
-#     print(match)
