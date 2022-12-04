4aaccb66fb12835a977a158352416b4aad4fab05
lawlikeed <lawlikeed@gmail.com>
2022-12-04 12:26:44 -0600

    making sure output file is .txt

--- imports.py
+++ imports.py
@@ -3,16 +3,19 @@ import itertools
 from math import floor
 import random
 import os
+import re
 from team import Team
 
 
-def round_robin(team_list, num_of_days, total_matches):
+def round_robin(team_list, num_of_days, total_matches, schedule_to_be_saved):
     for i in range(1, num_of_days+1):
+        schedule_to_be_saved = '-----Day',i,'line up-----\n'
         print('-----Day',i,'line up-----')
         for j in range(int(total_matches/num_of_days)):
             # Choose a random tuple from list of matches
             match = random.choice(team_list)
             # Display first team in tuple
+            schedule_to_be_saved = '' + str(match) + '\n'
             print(match)  
             # Update list  
             team_list.remove(match)
--- scheduler.py
+++ scheduler.py
@@ -5,6 +5,7 @@ num_of_days = 2#int(input ('How many days?: '))
 num_of_fields = 1#int(input ('How many fields?: '))
 total_matches = int(((num_of_teams/2) * (num_of_teams-1)))
 list_of_teams =[]
+schedule_to_be_saved = str()
 
 # Create generic team names based off of # of teams 
 for i in range(num_of_teams):
@@ -32,12 +33,21 @@ print(original_list , '\n')
 
 # Display matche line ups
 while True:
-    round_robin(original_list.copy(), num_of_days, total_matches)
+    schedule_to_be_saved = ''
+    round_robin(original_list.copy(), num_of_days, total_matches, schedule_to_be_saved)
     cont = input('Would you like to generate a new schedule? (Y/N): ')
     if cont.upper() == 'N':
         break
     os.system('cls')
 
+file_name = input('Give the schedule a name: ')
+
+if re.search('.txt$', file_name) == None:
+    file_name = file_name + '.txt'
+
+oFile = open(file_name, 'w')
+
+oFile.close()
 
 
 
